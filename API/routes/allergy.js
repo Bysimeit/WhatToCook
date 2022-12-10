76b6aadf34907b199362daf621228d9ef73c4fdd
BryceMo <etu45994@henallux.be>
2022-12-10 23:16:12 +0100

    fix allergy api v2

--- API/routes/allergy.js
+++ API/routes/allergy.js
@@ -5,7 +5,7 @@ const JWTMiddleWare = require("../middleware/identification");
 const Router = require("express-promise-router");
 const router = new Router;
 
-router.get('/',JWTMiddleWare.identification, AllergyController.getAllAllergy);
+router.get('/', AllergyController.getAllAllergy);
 router.post('/', JWTMiddleWare.identification, AuthoMiddleware.mustBeAdmin, AllergyController.postNewAllergy);
 router.patch('/', JWTMiddleWare.identification, AuthoMiddleware.mustBeAdmin, AllergyController.updateAllergy);
 router.delete('/',JWTMiddleWare.identification, AuthoMiddleware.mustBeAdmin, AllergyController.deleteAllergy);
