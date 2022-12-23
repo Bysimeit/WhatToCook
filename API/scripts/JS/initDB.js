c3d56f1aa5aec331fa36c177fe5d664aa7f14be6
Bysimeit <casanosimon@gmail.com>
2022-12-23 20:22:12 +0100

    Update initDB.js

--- API/scripts/JS/initDB.js
+++ API/scripts/JS/initDB.js
@@ -2,7 +2,6 @@ const pool = require("../../models/database");
 const fs = require("fs");
 const path = require("path");
 
-//traduire client ?
 async function initDB() {
     const client = await pool.connect();
     try {
